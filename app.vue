42d39ef337f4154d1036f7558f590bc3b4957aa1
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2022-07-02 18:10:28 +0200

    dependency cleanup

--- app.vue
+++ app.vue
@@ -2,6 +2,38 @@
 definePageMeta({
   layout: defualt,
 });
+
+// default head elements
+useHead({
+  title: 'Portfolio | Sam de Kanter',
+  meta: [
+    {
+      hid: 'description',
+      name: 'description',
+      content:
+        'Sam is a Front end Developer creating digital experiences currently studying Communication and multimedia design in Amsterdam.',
+    },
+    {
+      hid: 'ogdescription ',
+      property: 'og:description ',
+      content:
+        'Sam is a Front end Developer creating digital experiences currently studying Communication and multimedia design in Amsterdam.',
+    },
+    {
+      hid: 'ogtitle',
+      property: 'og:title',
+      content: 'Portfolio | Sam de Kanter',
+    },
+    {
+      hid: 'og:image',
+      property: 'og:image',
+      content: `http://schelpkikker.nl/logo.png`,
+    },
+  ],
+  htmlAttrs: {
+    lang: 'en',
+  },
+});
 </script>
 
 <template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,64 @@
-import { defineNuxtConfig } from "nuxt";
+import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ["@nuxt/content"],
+  modules: ['@nuxt/content'],
   content: {
     highlight: {
-      theme: "light-plus",
+      theme: 'light-plus',
     },
   },
 
-  /* Global CSS */
-  css: ["~/assets/scss/main.scss"],
+  router: {
+    base: '/',
+  },
+
+  /* global CSS */
+  css: ['~/assets/scss/main.scss'],
+
+  /* build modules */
+  buildModules: ['@nuxtjs/pwa'],
+
+  // might be broken?
+  pwa: {
+    workbox: {
+      /* workbox options */
+    },
+  },
+
+  /* default head */
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      meta: [{ name: 'theme-color', content: '#141414' }],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/apple-touch-icon.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: '/favicon-32x32.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: '/favicon-16x16.png',
+        },
+        { rel: 'manifest', href: '/site.webmanifest' },
+
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+        {
+          rel: 'syltesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;500;600;700&display=swap',
+        },
+      ],
+    },
+  },
 });
