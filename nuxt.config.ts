10d9f0b0ae0c77ebf7549fc954d13fb80140d00b
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2022-07-06 21:38:23 +0200

    localize font because speeed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -53,13 +53,6 @@ export default defineNuxtConfig({
           href: '/favicon-16x16.png',
         },
         { rel: 'manifest', href: '/site.webmanifest' },
-
-        { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
-        {
-          rel: 'syltesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;500;600;700&display=swap',
-          // get these from
-        },
       ],
     },
   },
