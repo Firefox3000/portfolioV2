78e59d480065b3aa2793822a6525485064565e77
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2022-07-07 23:24:53 +0200

    move error page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  target: 'server',
+  target: 'static',
   ssr: true,
 
   modules: ['@nuxt/content'],
