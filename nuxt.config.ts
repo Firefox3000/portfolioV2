010a36d1242270a3580ac68fcd1f6be795816b08
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2022-07-07 21:30:25 +0200

    try ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,8 @@ import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  target: 'static',
-  ssr: false,
+  target: 'server',
+  ssr: true,
 
   modules: ['@nuxt/content'],
 
