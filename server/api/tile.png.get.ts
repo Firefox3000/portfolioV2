42caadd96aecef0295edbe48d053623dabb2a903
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2022-12-04 17:13:06 +0100

    get file from server

--- server/api/tile.png.get.ts
+++ server/api/tile.png.get.ts
@@ -1,133 +1,63 @@
-// import { createCanvas, loadImage } from 'canvas';
 import { createCanvas, loadImage } from '@napi-rs/canvas';
-import { promises as fs } from 'node:fs';
-import path from 'node:path';
+// import { promises as fs } from 'node:fs';
+// import path from 'node:path';
 
 export default defineEventHandler(async (event) => {
-  // const { req, res } = event;
   const query = getQuery(event);
-  console.log('query', query);
 
-  // console.log(path.relative('/', './'));
-  // console.log(path.join('./'), path.relative('/', ''));
-
-  //Find the absolute path of the json directory
-  // const tileDir = path.join(process.cwd(), 'public/tile.jpg');
-
-  // console.log(process.cwd());
-  //Read the json data file data.json
-  // const fileContents = await fs.readFile(tileDir + 'tile.jpg');
-
-  // console.log(await fs.readdir(process.cwd()));
-  // console.log('---------');
-  // // console.log(await fs.readdir('./'));
-  // console.log('---------');
-  // console.log(await fs.readdir('/'));
-  // console.log('---------');
-  // console.log(await fs.readdir('../'));
-  // console.log('---------');
-  // console.log(await fs.readdir('/opt/'));
-  // console.log('---------');
-  // console.log(await fs.readdir('../opt'));
-  // console.log(await fs.readdir('../opt/public'));
-
-  const getFileList = async (dirName: string) => {
-    let files: string[] = [];
-
-    try {
-      const items = await fs.readdir(dirName, { withFileTypes: true });
-
-      for (const item of items) {
-        if (item.isDirectory() && !item.name.includes('node_modules')) {
-          files = [...files, ...(await getFileList(`${dirName}/${item.name}`))];
-        } else {
-          files.push(`${dirName}/${item.name}`);
-        }
-      }
-    } catch (error) {}
-
-    return files;
-  };
-
-  const files = await getFileList('/');
-  console.log(files);
-
-  // const jsonDirectory = path.join(process.cwd(), 'assets');
-  //Read the json data file data.json
-  // const fileContents = await fs.readFile(jsonDirectory + '/data.json', 'utf8');
-
-  // fs.readdir(process.cwd(), () => {
-  //   files.forEach((file) => {
-  //     console.log(file);
-  //   });
-  // });
-
-  // const tileBuffer = await fs.readFileSync(tileDir);
-  // console.log('found tile', !!tileBuffer);
-
-  // const fontSize = 72;
-  // const lineHeight = fontSize + 6;
+  const fontSize = 72;
+  const lineHeight = fontSize + 6;
 
   const imageSize = 800;
-  // const maxWidth = 540;
+  const maxWidth = 540;
 
-  // let words = query.content || '';
-  // console.log(words);
+  let words = query.content || '';
+  words = words.split(' ');
 
-  // // create canvas same width and height as image
+  // create canvas same width and height as image
   const canvas = createCanvas(imageSize, imageSize);
   const ctx = canvas.getContext('2d');
 
-  ctx.beginPath();
-  ctx.rect(20, 20, 150, 100);
-  ctx.stroke();
+  // would be better if image was loaded through fs
+  const tile = await loadImage(
+    'https://github.com/Vuurvos1/portfolio/blob/feat/tile/public/tile.jpg?raw=true'
+  );
+  ctx.drawImage(tile, 0, 0, imageSize, imageSize);
 
-  // const bg = await loadImage(tileDir);
-  // ctx.drawImage(bg, 0, 0, imageSize, imageSize);
+  ctx.font = `400 ${fontSize}px Serif`;
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'middle';
+  ctx.fillStyle = '#0f1b65';
 
-  // ctx.font = `400 ${fontSize}px Serif`;
-  // ctx.textAlign = 'center';
-  // ctx.textBaseline = 'middle';
-  // ctx.fillStyle = '#0f1b65';
+  // check line amount
+  let line = '';
+  let lines = [];
 
-  // // check line amount
-  // let line = '';
-  // let lines = [];
+  for (const word of words) {
+    let testLine = `${line} ${word}`;
+    let testWidth = ctx.measureText(testLine).width;
 
-  // for (let i = 0; i < words.length; i++) {
-  //   let testLine = `${line} ${words[i]}`;
-  //   let testWidth = ctx.measureText(testLine).width;
+    if (testWidth > maxWidth) {
+      // go to next line
+      lines.push(line.trim());
+      line = word;
+    } else {
+      line = testLine;
+    }
+  }
 
-  //   if (testWidth > maxWidth) {
-  //     // go to next line
-  //     lines.push(line.trim());
-  //     line = words[i];
-  //   } else {
-  //     line = testLine;
-  //   }
-  // }
+  lines.push(line);
 
-  // lines.push(line);
+  // fill tile with text
+  let startX = 0;
+  let startY = imageSize / 2 - ((lines.length - 1) * lineHeight) / 2 + 24;
 
-  // // fill tile with text
-  // let startX = 0;
-  // let startY = imageSize / 2 - ((lines.length - 1) * lineHeight) / 2 + 24;
-
-  // for (let i = 0; i < lines.length; i++) {
-  //   startX = imageSize / 2;
-
-  //   ctx.fillText(lines[i], startX, startY);
-
-  //   startY += lineHeight;
-  // }
-
-  // event.res.end(canvas.toBuffer('image/png', 100));
+  for (const line of lines) {
+    startX = imageSize / 2;
+    ctx.fillText(line, startX, startY);
+    startY += lineHeight;
+  }
 
   event.res.writeHead(200, { 'Content-Type': 'image/jpeg' });
-  // res.setHeader("content-type", 'image/')
-  return event.res.end(await canvas.encode('jpeg', 80));
-  // return event.res.end({ foo: 'bar' });
-
-  // return res;
-  // return event.res;
+  event.res.end(canvas.toBuffer('image/jpeg', 100));
 });
