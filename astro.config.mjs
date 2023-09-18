05cab533c7e1fb99e5f953d1e96d5d98d8e39db4
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2023-09-18 23:18:23 +0200

    sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import svelte from '@astrojs/svelte';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), svelte()],
+  site: process.env.VERCEL_URL,
+  integrations: [tailwind(), mdx(), svelte(), sitemap()]
 });
