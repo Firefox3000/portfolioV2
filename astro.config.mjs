f7e0f993c0c7474ea839dd9daa270c3ae7397ea3
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2023-09-19 23:26:53 +0200

    change config url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import svelte from '@astrojs/svelte';
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.VERCEL_URL,
-  integrations: [tailwind(), mdx(), svelte(), sitemap()]
+	site: 'https://schelpkikker.nl',
+	integrations: [tailwind(), mdx(), svelte(), sitemap()]
 });
